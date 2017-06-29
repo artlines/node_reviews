cac1b79da5f79c6e09ea149d7733d6b215c423e3
artlines <luart004@gmail.com>
2017-06-29 18:27:48 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ACTIVE: 1,
+  PRODUCTION_DB : 'db',
+  TEST_DB : 'db',
+  MODE_TEST : 'mode_test',
+  MODE_PRODUCTION : 'mode_production',
+  USER_NAME: 12,
+  REVIEW_CREATE_SUCCESS: 'Отзыв успешно добавлен и ждет модерации!',
+  REVIEW_EDIT_SUCCESS: 'Отзыв успешно отредактирован'
+};
